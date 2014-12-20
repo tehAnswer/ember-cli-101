29a766463f8d3a85c3c8c52934b9dd826a34442c
Sergio G <sergiorodriguezgijon@gmail.com>
2014-12-20 23:11:20 +0100

    Edit form and refactor

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.resource('friends', function() {
   	this.route('new');
   	this.route('show', { path: ':friend_id' });
+  	this.route('edit', { path: ':friend_id/edit'});
   });
   
 });
