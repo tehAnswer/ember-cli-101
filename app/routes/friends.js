f4f42f666bbcd793de01df9a8c8b844cd26aa146
Sergio G <sergiorodriguezgijon@gmail.com>
2014-12-20 23:33:29 +0100

    Delete records

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		delete: function(friend) {
+			friend.destroyRecord();
+			this.transitionTo('friends.index');
+		}
+	}
 });
