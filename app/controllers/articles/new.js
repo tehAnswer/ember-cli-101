4508b4b0ac2622be7c65ae60430a8c49934cd595
Sergio G <sergiorodriguezgijon@gmail.com>
2014-12-21 18:18:18 +0100

    Lending articles

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	isValid: Ember.computed('description', function() {
+		return !Ember.isEmpty(this.get('model.description'));
+	}),
+	actions: {
+    save: function() {
+    	 if(this.get('isValid'))
+    	 	return true;
+    	 else
+    	 	this.set('errorMessage', 'Fill the description omfg.');
+    }
+	}
+});
---
+++ app/routes/articles/new.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model: function() {
+	  return this.store.createRecord('article', {
+	    friend: this.modelFor('friends/show')
+	  });
+	},
+	actions: {
+		save: function() {
+			var _this = this;
+
+			this.modelFor('articles/new').save().then(function() {
+				_this.transitionTo('articles');
+			});
+		},
+		cancel: function() {
+			this.transitionTo('articles');
+		}
+	}
+});
